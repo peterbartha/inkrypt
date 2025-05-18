9a39178eca8c3cbb4d1a580162bf81d927d8aa15
Peter Bartha <peterbartha@pm.me>
2025-05-18 22:28:58 +0200

    chore: add Prettier for code formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('prettier').Config}
+ */
+export default {
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
